7195c63ae75bc6e8c0b4d3efeff3f151e71c1dfe
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-12-25 20:47:38 +0400

    doing

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     getWeather('Izhevsk');
 });
 
-
-
 function getWeather(city) {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=d6e481919bcce169184f16536bb64bd8&units=metric')
         .then(response => response.json())
